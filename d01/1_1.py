ef5b6b3a2e7faf5e5915effea4cbb54f059a76df
szeko34 <42772637+szeko34@users.noreply.github.com>
2021-12-01 06:35:37 +0100

    Update 1_1.py

--- d01/1_1.py
+++ d01/1_1.py
@@ -4,8 +4,8 @@ subor=open("1.txt","r")
 
 cisla=[int(x) for x in subor]
 vacsie = 0
-for i in range(3,len(cisla)):
-    if cisla[i]>cisla[i-3]:
+for i in range(1,len(cisla)):
+    if cisla[i]>cisla[i-1]:
         vacsie+=1
 
 print(vacsie)
